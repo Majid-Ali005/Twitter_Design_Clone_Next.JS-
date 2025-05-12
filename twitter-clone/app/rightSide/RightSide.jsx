9b498562b3c5f46313962d5614ec03904334eeb9
M. Majid Ali <147548345+Majid-Ali005@users.noreply.github.com>
2025-05-12 16:56:08 +0000

    .

--- twitter-clone/app/rightSide/RightSide.jsx
+++ twitter-clone/app/rightSide/RightSide.jsx
@@ -14,69 +14,92 @@ const RightSide = () => {
     <input type="text" placeholder="Search Twitter" className="bg-gray-300 px-[40px] py-2 rounded-4xl relative" />
 
 <br /> <br />
-    <div className="bg-gray-200 w-[400px] h-[400px] rounded-xl">
+    <div className="bg-gray-200 w-[300px] h-[310px] rounded-xl">
 
 <div className="ml-5 mt-3 text-xl font-bold absolute">
     <h3>Trends for you</h3>
     <img 
     src="https://static-00.iconduck.com/assets.00/settings-icon-2048x2046-cw28eevx.png"
-    width={20}
-    className="ml-[330px] mt-[-15px]"
+    width={18}
+    className="ml-[240px] mt-[-18px]"
     
     />
 </div>
 
-<div className="ml-5 mt-3  mt-[65px] text-[-50px] absolute">
-   <p>Trends in World  </p>
+<div className="ml-5 mt-3  mt-[65px] text-sm/8 absolute">
+    <p>Trends in World 12  </p>
    
-   <p> #newgraduated</p>
+   <p className="mt-[-15px]"> #newgraduated</p>
     
-   <p> 15222 Tweets</p>
+   <p className="mt-[-15px]"> 15222 Tweets</p>
    
     <img 
     src="https://static.thenounproject.com/png/1380510-200.png"
     width={20}
-    className="ml-[330px] mt-[-65px]"
+    className="ml-[240px] mt-[-75px]"
     
     />
 </div>
-<br /><br /><br /><br />
+<br /><br /> <br />
 
-<div className="ml-5 mt-3  mt-[65px] text-[-50px] absolute">
-   <p>Trends in World 
+<div className="ml-5 mt-3  mt-[65px] text-sm/8 absolute">
+   <p>Trends in World 12  </p>
    
-   <p> #newgraduated</p>
+   <p className="mt-[-15px]"> #newgraduated</p>
     
-   <p> 15222 Tweets</p>
-   </p>
+   <p className="mt-[-15px]"> 15222 Tweets</p>
+   
     <img 
     src="https://static.thenounproject.com/png/1380510-200.png"
     width={20}
-    className="ml-[330px] mt-[-65px]"
+    className="ml-[240px] mt-[-75px]"
     
     />
 </div>
 
-<br /><br /><br /><br />
+<br /><br /><br />
 
-<div className="ml-5 mt-3  mt-[65px] text-[-50px] absolute">
-   <p>Trends in World 
+<div className="ml-5 mt-3  mt-[65px] text-sm/8 absolute">
+    <p>Trends in World 12  </p>
    
-   <p> #newgraduated</p>
+   <p className="mt-[-15px]"> #newgraduated</p>
     
-   <p> 15222 Tweets</p>
-   </p>
+   <p className="mt-[-15px]"> 15222 Tweets</p>
+   
     <img 
     src="https://static.thenounproject.com/png/1380510-200.png"
     width={20}
-    className="ml-[330px] mt-[-65px]"
+    className="ml-[240px] mt-[-75px]"
     
     />
 </div>
 <br /><br /><br /><br /><br /><br /><br />
-<Link href="#"><p className="ml-5 text-sky-600">Show More....</p></Link>
+<Link href="#"><p className="ml-5 text-sky-600 mt-[-35px]">Show More....</p></Link>
 
     </div>
+
+<br />
+
+{/* profile like div */}
+<div className="bg-gray-200 wi-[400px] h-[135px] rounded-2xl">
+<h1 className="text-xl font-bold ml-5 mt-2 absolute ">who to follow</h1>
+
+<div className="mt-[50px] ml-5 absolute">
+
+<img
+src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSj7j9IDpZsbq4HghrNPneZustxYupRgrt0oQ&s"
+width={35}
+/>
+
+<div className="ml-[50px] mt-[-42px]">
+        <p>Majid Ali</p>
+<p>@majidali</p>
+</div>
+       <button type="button" className=" ml-[190px] mt-[-50px] absolute text-white bg-indigo-500 hover:bg-black-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-4xl text-sm px-5 py-1.5 me-2 mb-2 dark:bg-black dark:hover:bg-black-700 focus:outline-none dark:focus:ring-blue-800">Follow</button>
+<p className="text-sky-500 mt-1">Show More...</p>
+
+</div>
+</div>
 
    </div>
     </>
